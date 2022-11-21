acdb7ca59482119d60f6dc4e56187a7ee4f19bff
alaminhossain171 <48113312+alaminhossain171@users.noreply.github.com>
2022-11-21 13:25:21 +0600

    bug -fixed

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import Routes from './src/navigations/routes'
+
 import ParentRoute from './src/navigations/ParentRoute'
 
 const App = () => {
